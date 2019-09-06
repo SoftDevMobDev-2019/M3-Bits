99e405729269f2c0bfa70b4347bf24a8aeaf5e1f
N. A. Ronald <naronald@users.noreply.github.com>
2019-09-06 14:14:07 +1000

    Added Espresso test

---
+++ app/src/androidTest/java/au/edu/swin/sdmd/bits/MainActivityTest.java
@@ -0,0 +1,58 @@
+package au.edu.swin.sdmd.bits;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction input = onView(withId(R.id.etDecimal));
+        input.perform(replaceText("42"));
+
+        ViewInteraction button = onView(withId(R.id.button));
+        button.perform(click());
+
+        ViewInteraction output = onView(withId(R.id.tvBinary));
+        output.check(matches(withText("101010")));
+
+        /* If add is used for the fragment, this picks up that there are multiple output views
+        on screen.
+         */
+
+        input.perform(replaceText("10"));
+        button.perform(click());
+        output.check(matches(withText("1010")));
+
+
+    }
+}
